207ce37240be87b3cd027afaf2c394aef694e2be
Damian Godek <damian.godek1@gmail.com>
2018-09-27 21:33:50 +0200

    add carousel with functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-var elem = document.querySelector('.main-carousel');
+
+var elem = document.querySelector('.carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
@@ -7,6 +8,24 @@ var flkty = new Flickity( elem, {
 
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.main-carousel', {
+var flkty = new Flickity( '.carousel', {
   // options
+
 });
+
+var flkty = new Flickity('.carousel');
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
+
+var $carousel = $('.carousel').flickity();
+
+$('.button-group').on( 'click', '.button', function() {
+  var index = $(this).index();
+  $carousel.flickity( 'select', index );
+});
+
