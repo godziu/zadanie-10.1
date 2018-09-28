e8264d543a83a5543e6a6745771b0ac1402ce5a1
Damian Godek <damian.godek1@gmail.com>
2018-09-28 14:49:53 +0200

    add 3 markers to map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var tmplMain = document.getElementById('main-carousel').innerHTML;
 var tmplItm = document.getElementById('template-items').innerHTML;
@@ -37,10 +38,42 @@ flkty.on( 'scroll', function( progress ) {
   progressBar.style.width = progress * 100 + '%';
 });
 
-// var $carousel = $('.carousel').flickity();
+var infos = document.getElementById('infos');
+window.initMap = function() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  var coords2 = {lat: -25.363, lng: 134.044};
+  var coords3 = {lat: -25.363, lng: 137.044};
 
-// $('.button-group').on( 'click', '.button', function() {
-//   var index = $(this).index();
-//   $carousel.flickity( 'select', index );
-// });
 
+  // The map, centered at Uluru
+  var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
+
+  var markerOne = new google.maps.Marker({
+			position: uluru,
+			map: map
+		});
+
+  markerOne.addListener('click', function(){
+  	infos.innerHTML = 'you clicked markerOne';
+
+		});
+
+  var markerTwo = new google.maps.Marker({
+			position: coords2,
+			map: map
+		});
+
+  markerTwo.addListener('click', function(){
+			infos.innerHTML = 'You clicked markerTwo';
+		});
+  
+  var markerThree = new google.maps.Marker({
+			position: coords3,
+			map: map
+		});
+
+  markerThree.addListener('click', function(){
+			infos.innerHTML = 'You clicked markerThree';
+		});
+}
