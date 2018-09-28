b9ef773d342fbc2cbd097c071bd2cc17bd1b736a
Damian Godek <damian.godek1@gmail.com>
2018-09-28 16:14:21 +0200

    add map zoom/smoothPan

--- js/script.js
+++ js/script.js
@@ -29,6 +29,7 @@ var flkty = new Flickity( '.carousel', {
 
 });
 
+
 var flkty = new Flickity('.carousel');
 
 var progressBar = document.querySelector('.progress-bar')
@@ -38,13 +39,68 @@ flkty.on( 'scroll', function( progress ) {
   progressBar.style.width = progress * 100 + '%';
 });
 
+// ====================================================
+
+var smoothPanAndZoom = function(map, zoom, coords){
+	var jumpZoom = zoom - Math.abs(map.getZoom() - zoom);
+		jumpZoom = Math.min(jumpZoom, zoom -1);
+		jumpZoom = Math.max(jumpZoom, 3);
+		smoothZoom(map, jumpZoom, function(){
+			smoothPan(map, coords, function(){
+				smoothZoom(map, zoom); 
+			});
+		});
+	};
+
+var smoothZoom = function(map, zoom, callback) {
+		var startingZoom = map.getZoom();
+		var steps = Math.abs(startingZoom - zoom);
+
+if(!steps) {
+	if(callback) {
+		callback();
+		}
+	return;
+}
+
+var stepChange = - (startingZoom - zoom) / steps;
+
+var i = 0;
+
+var timer = window.setInterval(function(){
+	if(++i >= steps) {
+		window.clearInterval(timer);
+		if(callback) {
+			callback();
+			}
+}
+
+map.setZoom(Math.round(startingZoom + stepChange * i));
+		}, 80);
+	};
+
+// =====================================================
+
+
 var infos = document.getElementById('infos');
+
 window.initMap = function() {
   // The location of Uluru
   var uluru = {lat: -25.344, lng: 131.036};
   var coords2 = {lat: -25.363, lng: 134.044};
   var coords3 = {lat: -25.363, lng: 137.044};
 
+  document.getElementById('center-map').addEventListener('click', function(event){
+	event.preventDefault();
+	map.panTo(coords2);
+	map.setZoom(10);
+});
+
+  document.getElementById('center-smooth').addEventListener('click', function(event){
+	event.preventDefault();
+	smoothPanAndZoom(map, 7, uluru);
+});
+		
 
   // The map, centered at Uluru
   var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
